ef40982c437815ff073b845c17f24b5a82122b71
anthonphax <anthonphax@gmail.com>
2021-10-11 15:47:56 -0300

    versão_1.0
    uma estrutura lógica inicial

--- v1/models.py
+++ v1/models.py
@@ -5,13 +5,18 @@ from django.contrib.auth.models import User
 from django.db import models
 import uuid
 
-#modelos de usuários
-
 gender_choices = (
         ("M", "Masculino"),
         ("F", "Feminino"),
         ("NA", "Outro/Não declarado")
     ) 
+
+item_status = (
+    ("D", "Disponível"),
+    ("I", "Indisponível"),
+    ("M", "Em Manutenção"),
+    ("A", "Atrasado")
+    )
 
 class Usuario:
     class Aluno(models.Model): #todos os modelos são classes python que herdam a classe models
@@ -47,6 +52,28 @@ class Usuario:
             max_length=105,
             default="NA"
             )
+        points = models.IntegerField(
+            name="Pontos",
+            default=0
+        )
+
+        def level(self): #game
+            if self.points < 75:
+                return 1
+            elif 75 >= self.points < 150:
+                print("in training")
+                return 2
+            elif 150 >= self.points < 300:
+                print("gear")
+                return 3
+            elif 600 >= self.points < 600:
+                print("machine")
+                return 4
+            elif self.points >= 600:
+                print("veteran")
+                return 5
+            else:
+                print("algo errado")
 
         def __str__(self):
             return self.user.username
@@ -150,7 +177,6 @@ class Usuario:
             default="NA"
             )
 
-
 class Item:
     class Livro(models.Model):
         id = uuid.uuid1().hex #32
@@ -161,22 +187,98 @@ class Item:
         description = models.TextField()
         created = models.DateTimeField(auto_now_add=True)
         updated = models.DateTimeField(auto_now=True)
+        status = models.CharField(
+            max_length=255,
+            choices=item_status,
+            default="D"
+        )
 
         def __str__(self):
             return str(self.title + " " + self.author + " " + self.language)
 
     class MidiaDigital(models.Model): #CD, DVD, pen-drive
-        pass
+        id = uuid.uuid1().hex
+        title = models.CharField(max_length=255) 
+        slug = models.SlugField(max_length=255, unique=True)
+        created = models.DateTimeField(auto_now_add=True)
+        updated = models.DateTimeField(auto_now=True)
+        description = models.TextField()
+        status = models.CharField(
+            max_length=255,
+            choices=item_status,
+            default="D"
+        )
 
     class TrabalhoAcademico(models.Model): #artigos, monografias 
-        pass
+        id = uuid.uuid1().hex
+        title = models.CharField(max_length=255)
+        author = models.CharField(max_length=50) 
+        slug = models.SlugField(max_length=255, unique=True)
+        created = models.DateTimeField(auto_now_add=True)
+        updated = models.DateTimeField(auto_now=True)
+        description = models.TextField()
+        status = models.CharField(
+            max_length=255,
+            choices=item_status,
+            default="D"
+        )
 
     class Modelo(models.Model): #portfólios, mapas, plantas baixas
-        pass
-
+        id = uuid.uuid1().hex
+        title = models.CharField(max_length=255)
+        slug = models.SlugField(max_length=255, unique=True)
+        created = models.DateTimeField(auto_now_add=True)
+        updated = models.DateTimeField(auto_now=True)
+        description = models.TextField()
+        status = models.CharField(
+            max_length=255,
+            choices=item_status,
+            default="D"
+        )
     class Dispositivo(models.Model): #projetores, computadores
+        id = uuid.uuid1().hex
+        title = models.CharField(max_length=255)
+        slug = models.SlugField(max_length=255, unique=True)
+        created = models.DateTimeField(auto_now_add=True)
+        updated = models.DateTimeField(auto_now=True)
+        description = models.TextField() 
+        status = models.CharField(
+            max_length=255,
+            choices=item_status,
+            default="D"
+        )
+    class Periferico(models.Model): #cabos, adaptadores, etc
+        id = uuid.uuid1().hex
+        title = models.CharField(max_length=255)
+        slug = models.SlugField(max_length=255, unique=True)
+        created = models.DateTimeField(auto_now_add=True)
+        updated = models.DateTimeField(auto_now=True)
+        description = models.TextField()
+        status = models.CharField(
+            max_length=255,
+            choices=item_status,
+            default="D"
+        )
+
+    def emprestimo(self):
         pass
 
-    class Periferico(models.Model): #cabos, adaptadores, etc
+    def devolucao(self):
+        # if self.status != "A":
+        #     self.points += 5
+        #     self.status = "D"
+        # else:
+        #     self.status = "D"
         pass
+    def manutencao(self):
+        pass 
             
+class Relatorio:
+    class Demanda(models.Model):
+        pass
+    class Estoque(models.Model):
+        pass
+    class Danos(models.Model):
+        pass
+    class Previsão(models.Model):
+        pass
